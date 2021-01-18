232a6e95152f6413e47262786849f4bf29952196
bohdanmykhayliv <bohdan.mykhailiv@ucu.edu.ua>
2021-01-18 12:29:44 +0200

    Some changes in main.dart

--- first_app_on_course/lib/main.dart
+++ first_app_on_course/lib/main.dart
@@ -5,7 +5,9 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello!'),);
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(),
+    ),);
   }
 
 }
